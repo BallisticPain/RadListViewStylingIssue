e3cb6e0fc683186c354ee62227e0794abe521b86
Jarvis (BallisticPain) Stubblefield <jarvis@vortexrevolutions.com>
2021-04-05 17:52:00 -0500

    Added RadListView to startup Angular NativeScript Sample App.

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core'
+import { View } from '@nativescript/core'
+import { ListViewEventData } from "nativescript-ui-listview"
 
 import { Item } from './item'
 import { ItemService } from './item.service'
@@ -6,6 +8,7 @@ import { ItemService } from './item.service'
 @Component({
   selector: 'ns-items',
   templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css'],
 })
 export class ItemsComponent implements OnInit {
   items: Array<Item>
@@ -14,5 +17,41 @@ export class ItemsComponent implements OnInit {
 
   ngOnInit(): void {
     this.items = this.itemService.getItems()
+  }
+
+  onCellSwiping(args: ListViewEventData) {
+    const swipeLimits = args.data.swipeLimits;
+    const currentItemView = args.object;
+
+    if (args.data.x > 200) {
+      console.log("Notify perform left action");
+    } else if (args.data.x < -200) {
+      console.log("Notify perform right action");
+    }
+  }
+
+  onSwipeCellStarted(args: ListViewEventData) {
+    console.log('Cell swipe started');
+    const swipeLimits = args.data.swipeLimits;
+    const swipeView = args['object'];
+    const leftItem = swipeView.getViewById<View>('left-view');
+    const rightItem = swipeView.getViewById<View>('right-view');
+    swipeLimits.left = leftItem.getMeasuredWidth();
+    swipeLimits.right = rightItem.getMeasuredWidth();
+    swipeLimits.threshold = leftItem.getMeasuredWidth() / 2;
+  }
+
+  onSwipeCellFinished(args: ListViewEventData) {
+    console.log('Cell swipe finished');
+  }
+
+  onLeftSwipeClick(args: ListViewEventData) {
+    // Left Swipe is to Open the Job
+    console.log('Left Swipe Click', args.index);
+  }
+
+  onRightSwipeClick(args: ListViewEventData) {
+    // Right Swipe is to View Summary
+    console.log('Right Swipe Click', args.index, args.data);
   }
 }
